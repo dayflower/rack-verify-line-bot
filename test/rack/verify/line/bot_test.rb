a61323b7c95c9134acf4ce31ddae86a87d8cc94c
ITO Nobuaki <daydream.trippers@gmail.com>
2016-12-13 22:10:29 +0900

    Add tests
    This fixes #2

--- test/rack/verify/line/bot_test.rb
+++ test/rack/verify/line/bot_test.rb
@@ -1,11 +1,68 @@
 require 'test_helper'
+require 'stringio'
 
-class Rack::Verify::Line::BotTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Rack::Verify::Line::Bot::VERSION
+class Rack::Verify::Line::BotTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  PAYLOAD = StringIO.new(%q({"events":[{"type":"message","replyToken":"75e6cb6a6dc14b2c8d25eb1fda06bc6c","source":{"userId":"Ua20fb80218abd1686411dec7f2b977a0","type":"user"},"timestamp":1481632823600,"message":{"type":"text","id":"5344013044158","text":"test"}}]}))
+
+  VALID_SIGNATURE   = 'dX6QKJqcdyDfyi3k6LQwO65nEZR5ZLr0djyccy3iayA='
+  INVALID_SIGNATURE = 'InvalidSignatureInvalidSignatureInvalidSign='
+
+  def app
+    bare_app = ->(env) {
+      [ 204, [], [] ]
+    }
+
+    Rack::Verify::Line::Bot.new(
+      bare_app,
+      :secret => '80eb366d64ab27645880b808ca672635',
+      :path   => '/webhook',
+    )
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_get
+    get '/'
+
+    assert last_response.successful?
+  end
+
+  def test_post_to_non_webhook
+    post '/', PAYLOAD, {
+      'CONTENT_TYPE'          => 'application/json;charset=UTF-8',
+      'CONTENT_LENGTH'        => PAYLOAD.length.to_s,
+      'HTTP_X_LINE_SIGNATURE' => INVALID_SIGNATURE,
+    }
+
+    assert last_response.successful?
+  end
+
+  def test_without_signature
+    post '/webhook', PAYLOAD, {
+      'CONTENT_TYPE'          => 'application/json;charset=UTF-8',
+      'CONTENT_LENGTH'        => PAYLOAD.length.to_s,
+    }
+
+    assert last_response.bad_request?
+  end
+
+  def test_with_invalid_signature
+    post '/webhook', PAYLOAD, {
+      'CONTENT_TYPE'          => 'application/json;charset=UTF-8',
+      'CONTENT_LENGTH'        => PAYLOAD.length.to_s,
+      'HTTP_X_LINE_SIGNATURE' => INVALID_SIGNATURE,
+    }
+
+    assert last_response.bad_request?
+  end
+
+  def test_with_valid_signature
+    post '/webhook', PAYLOAD, {
+      'CONTENT_TYPE'          => 'application/json;charset=UTF-8',
+      'CONTENT_LENGTH'        => PAYLOAD.length.to_s,
+      'HTTP_X_LINE_SIGNATURE' => VALID_SIGNATURE,
+    }
+
+    assert last_response.successful?
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rack/verify/line/bot'
 
-require 'minitest/autorun'
+require 'test/unit'
+require 'rack/test'
